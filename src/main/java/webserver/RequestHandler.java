10bc82db5b2074fb66f350f183e499d517deb8b9
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2024-04-04 15:04:30 +0900

    요구사항 1,2

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,5 +1,6 @@
 package webserver;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
@@ -7,6 +8,7 @@ import util.HttpRequestUtils;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class RequestHandler extends Thread{
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -24,12 +26,31 @@ public class RequestHandler extends Thread{
             InputStreamReader isr = new InputStreamReader(in,"UTF-8");
             BufferedReader br = new BufferedReader(isr);
             String url = HttpRequestUtils.getUrl(br.readLine());
+            int index = url.indexOf("?");
+            String requestPath;
+            String defaultPath="/index.html";
+            String params;
+            if (index==-1){
+             requestPath=url;
+             params="";
+            }
+            else {
+                requestPath = url.substring(0, index);
+                params = url.substring(index + 1);
+            }
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
-            response200Header(dos,body.length);
-            responseBody(dos,body);
-        } catch(Exception e){
+            if (Files.exists( Paths.get("./webapp"+requestPath))){
+            byte[] body = Files.readAllBytes(new File("./webapp"+requestPath).toPath());
+            urlFunction(requestPath,dos,params,body);
+            }
+            else{
+                log.debug("defaultPath{}",defaultPath);
+            byte[] body = Files.readAllBytes(new File("./webapp"+defaultPath).toPath());
+            urlFunction(requestPath, dos, params, body);
+             }
+            } catch(Exception e){
             e.printStackTrace();
+
         }
     }
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
@@ -43,6 +64,16 @@ public class RequestHandler extends Thread{
         }
     }
 
+    private void response302Header(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("Http/1.1 302 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: "+lengthOfBodyContent+"\r\n");
+            dos.writeBytes("\r\n");
+        }catch(IOException e){
+            log.error(e.getMessage());
+        }
+    }
     private void responseBody(DataOutputStream dos, byte[] body) {
         try {
             dos.write(body, 0, body.length);
@@ -52,4 +83,27 @@ public class RequestHandler extends Thread{
         }
     }
 
+    private void urlFunction(String requestPath, DataOutputStream dos, String params, byte[] body){
+        if("/user/create".equals(requestPath)) {
+            if (params==null) {
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
+            else{
+                String[] info=params.split("&");
+                String[] acc = new String[4];
+                for(int i=0; i<info.length;i++){
+                    String[] value = info[i].split("=");
+                    acc[i]=value[1];
+                }
+                User user = new User(acc[0],acc[1],acc[2],acc[3]);
+                response302Header(dos,body.length);
+                responseBody(dos, body);
+            }
+        }
+        else{
+            response200Header(dos, body.length);
+            responseBody(dos, body);
+        }
+    }
 }
